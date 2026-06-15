9a724bb56f16c241e8ab8679ac823a6d9bec812a
yendinh2704 <hoangyen274@gmail.com>
2026-06-15 20:40:57 +0700

    commit code day5

--- tests/TextBoxTest.spec.ts
+++ tests/TextBoxTest.spec.ts
@@ -1,15 +1,22 @@
-import { test, expect } from '@playwright/test';
+// @ts-ignore
+const { TextBoxPage } = require('../pages/TextBoxPage');
+import { test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://demoqa.com/text-box');
-    await page.fill('#userName', 'John Doe');
-    await page.fill('#userEmail', 'john.doe@example.com');
-    await page.fill('#currentAddress', '123 Main St');
-    await page.fill('#permanentAddress', '456 Oak Ave');
-    await page.click('#submit');
-    await expect(page.locator('#name')).toContainText('John Doe');
-    await expect(page.locator('#email')).toContainText('john.doe@example.com');
-    await expect(page.locator('xpath=//p[@id="currentAddress"]')).toContainText('123 Main St');
-    await expect(page.locator('xpath=//p[@id="permanentAddress"]')).toContainText('456 Oak Ave');
+test.describe('TextBoxTests', () => {
+  let textBoxPage: any;
+  test.beforeEach(async ({ page }) => {
+    textBoxPage = new TextBoxPage(page);
+    await textBoxPage.goto();
+  });
+
+   test.afterEach(async () => {
+    await textBoxPage.removeAll();
+  });
+
+  test ('Submit successfully', async () => {
+    await textBoxPage.inputData('John Doe', 'john.doe@example.com', '123 Main St', '456 Oak Ave');  
+//todo: verify the output
+  });
+
+  
 });
- 
